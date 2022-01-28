342f0daf7cd7591dfd0031b49546cea7d62bb988
Robert Krency <bobkrency@gmail.com>
2022-01-28 11:55:41 -0500

    CSC423 - Assignment 1 work

--- CSC 323/Assignments/Assignment 1/G6P1.asm
+++ CSC 323/Assignments/Assignment 1/G6P1.asm
@@ -12,27 +12,81 @@ TITLE Average (Average.asm)
 INCLUDE Irvine32.inc
 
 .data
-msg_Details		BYTE	"Welcome to the grade accumulator.", 0
-msg_GetInput	BYTE	"Input an integer, 0-100 inclusive: ", 0
-msg_Count		BYTE	"Count: ", 0
-msg_Total		BYTE	"Total: ", 0
-msg_Average		BYTE	"Average: ", 0
 
-count			dword	0
-total			dword	0
-average			dword	0
+; 13, 10 characters are CR,LF to create a new line
+msg_Details				BYTE	"Welcome to the grade accumulator.", 13, 10, 0
+msg_QuitDetails			BYTE	"To quit, enter two invalid inputs in a row.", 13, 10, 0
+msg_GetInput			BYTE	"Input an integer, 0-100 inclusive: ", 0
+msg_Count				BYTE	13, 10, "Count: ", 0
+msg_Total				BYTE	13, 10, "Total: ", 0
+msg_Average				BYTE	13, 10, "Average: ", 0
+msg_err_NumRange		BYTE	13, 10, "Error: Number not within range 0-100, inclusive.", 13, 10 0
+
+count					dword	0
+total					dword	0
+average					dword	0
+
+buffer					BYTE	21 DUP(0)
+byteCount				dword	?
+
+flag_Error				dword	0		; Tracks if an error has occurred previously
 
 .code
 main PROC
-
-	mov edx, OFFSET msg_Details
-	call WriteString
-
-GetInput:
-	mov edx, OFFSET msg_GetInput
+	mov edx, OFFSET msg_Details			; Display welcome message
 	call WriteString
 	
+	mov edx, OFFSET msg_QuitDetails		; Display how to quit
+	call WriteString
 
+	
+GetInput:
+	mov edx, OFFSET msg_GetInput		; Prompt user for input
+	call WriteString
+	
+	mov edx, OFFSET buffer				; Read input from user as string
+	mov ecx, SIZEOF buffer
+	call ReadString
+	mov byteCount, eax
+
+	mov edx, OFFSET buffer				; Parse input string as integer
+	mov ecx, byteCount
+	call ParseInteger32
+
+	; Need to check for the overflow error for bad input
+	; Need to check if number is between 0 and 100
+
+	mov ebx, total						; Add the new number to the total
+	add ebx, eax
+	mov total, ebx
+
+	mov ebx, count						; Increment the counter
+	add ebx, 1
+	mov count, ebx
+
+	; This loop keeps crashing out randomly?
+	loop GetInput
+
+
+CalcAverage:
+	nop
+
+	
+PrintStats:
+	mov edx, OFFSET msg_Total				; Print out the Total
+	call WriteString
+	mov eax, total
+	call WriteInt
+
+	mov edx, OFFSET msg_Count				; Print out the Count
+	call WriteString
+	mov eax, count
+	call WriteInt
+
+	mov edx, OFFSET msg_Average				; Print out the Average
+	call WriteString
+	mov eax, average
+	call WriteInt
 
 
 	exit
