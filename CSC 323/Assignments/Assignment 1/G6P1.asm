41c12fefc1e7361e4f0577438b56352341f86cb4
Robert Krency <bobkrency@gmail.com>
2022-01-31 12:50:22 -0500

    CSC 323 - Assignment 1 finished.

--- CSC 323/Assignments/Assignment 1/G6P1.asm
+++ CSC 323/Assignments/Assignment 1/G6P1.asm
@@ -1,10 +1,10 @@
-TITLE Average (Average.asm)
+TITLE Average Grade Accumulator (G6P1.asm)				; OF DOOM
 
 ; CSC 323 - Group 6 - Assignment 1: Average
 ; Author: Robert Krency, kre1188@calu.edu
-; Author: Anthony Stepich, <email>
-; Author: Austin Pringle, <email>
-; Author: Camden Kovach, <email>
+; Author: Anthony Stepich, ste4864@calu.edu
+; Author: Austin Pringle, pri2679@calu.edu
+; Author: Camden Kovach, kov2428@calu.edu
 
 ; This program reads in integer grades from the user and compiles
 ; an average, total count, and total sum statistics of the input.
@@ -12,115 +12,168 @@ TITLE Average (Average.asm)
 INCLUDE Irvine32.inc
 
 .data
-msg_Details				BYTE	"Welcome to the grade accumulator.", 13, 10, 0
-msg_QuitDetails			BYTE	"To quit, enter two invalid inputs in a row.", 13, 10, 0
-msg_GetInput			BYTE	"Input an integer, 0-100 inclusive: ", 0
-msg_Count				BYTE	13, 10, "Count: ", 0
-msg_Total				BYTE	13, 10, "Total: ", 0
-msg_Average				BYTE	13, 10, "Average: ", 0
-msg_Remainder				BYTE    13, 10, "Remainder:",0
-msg_err_NumRange		BYTE	13, 10, "Error: Number not within range 0-100, inclusive.", 13, 10, 0
 
+; Output Strings
+msg_Details				BYTE	"Welcome to the grade accumulator.", 0
+msg_QuitDetails			BYTE	"To quit, enter a null value as first input each round.", 0
+msg_GetInput			BYTE	"Input an integer, 0-100 inclusive: ", 0
+msg_Count				BYTE	"Count: ", 0
+msg_Total				BYTE	"Total: ", 0
+msg_Average				BYTE	"Average: ", 0
+msg_Remainder			BYTE    "Remainder: ", 0
+msg_Quit				BYTE	"Exiting...", 0
+msg_err_NumRange		BYTE	"Error: Number not within range 0-100, inclusive.", 0
+
+; Grade Stats
 count					dword	0
 total					dword	0
 average					dword	0
 remainder				dword   0
 
+; Input Buffer
 buffer					BYTE	21 DUP(0)
 byteCount				dword	?
 
-
-flag_Error	dword	0		; Tracks if an error has occurred previously
-
+; Flags
+flag_FirstInput			dword	1			; Flag for if the first user input this round
 
 
 .code
 main PROC
-	mov edx, OFFSET msg_Details			; Display welcome message
+	mov edx, OFFSET msg_Details				; Display welcome message
 	call WriteString
+	call Crlf
 	
-	mov edx, OFFSET msg_QuitDetails		; Display how to quit
+	mov edx, OFFSET msg_QuitDetails			; Display how to quit
 	call WriteString
+	call Crlf
 
+
+GetInput:									
+	; Get user input
+
+	mov edx, OFFSET msg_GetInput			; Prompt user for input
+	call WriteString 
 	
-GetInput:
-	mov edx, OFFSET msg_GetInput		; Prompt user for input
-	call WriteString
-	
-	cmp 
-	
-	mov edx, OFFSET buffer				; Read input from user as string
+	mov edx, OFFSET buffer					; Read input from user as string
 	mov ecx, SIZEOF buffer
 	call ReadString
 	mov byteCount, eax
 
-	mov edx, OFFSET buffer				; Parse input string as integer
+	mov edx, OFFSET buffer					; Parse input string as integer
 	mov ecx, byteCount
-	call ParseInteger32
-	
-	cmp ecx,1
-	JO Get_input						;checks for overflow error
-	JNO L1
-	
-	L1:cmp ecx, 100
-	
-	JLE L2
-	L2:cmp ecx,0					;checks for valid number
-	JGE L3
 
-	
 
-	L3:mov ebx, total						; Add the new number to the total
+ValidateInput:
+	; Validate that the input is an integer between 0-100
+	; If not:
+	;		(1) If input is null and first input, quit
+	;		(2) If input is integer out of range, display error message
+	;		(3) If input is not an integer and not null, display error message
+	; If either of condition (2) or (3) is met, display the stats after the error message
+
+	cmp eax, 0									; Check for null input
+	JNE ValidateInteger
+
+	mov ebx, flag_FirstInput					; Check if this was the first input
+	cmp ebx, 1								
+	JE Quit										; If so, quit.
+
+	Call PrintStats								; Null Input, but not first input
+
+
+ValidateInteger:
+	; Validates that the input is an integer between 0 and 100, inclusive
+
+	call ParseInteger32							; Try to parse the input number
+	
+	JO PrintStats								; Checks for overflow error, indicating user input was not a valid integer
+	
+	cmp eax, 100								; Check if number is less than 100
+	JG PrintErrorNotInRange
+	cmp eax, 0									; Check if number is greater than 0
+	JL PrintErrorNotInRange
+
+	call AddNumbers								; Valid input
+
+
+PrintErrorNotInRange:
+	; Error for the input number not being between 0 and 100, inclusive
+
+	mov edx, OFFSET msg_err_NumRange
+	call WriteString
+	call Crlf
+	
+	call PrintStats								; Prints error message for input not in range 
+
+
+AddNumbers:
+	; Adds the validated user input to the stats
+
+	mov ebx, total								; Add the new number to the total
 	add ebx, eax
 	mov total, ebx
 
-	mov ebx, count						; Increment the counter
+	mov ebx, count								; Increment the counter
 	add ebx, 1
 	mov count, ebx
 
-	call GetInput						; Loop getting the input
-
-
-CalcAverage:
-	mov eax,total 
-	mov edx,count					;produces an average and remainder and assigns them both 
-	div edx
+	mov eax,total								; Calculate the Average using Integer division, storing the quotient and remainder
+	cdq
+	mov ebx,count								
+	div ebx
 	mov average,eax
 	mov remainder,edx
-	
-	
+
+	mov flag_FirstInput, 0						; Successful input, set first input flag to zero
+	call GetInput								; Loop getting the input
 
 	
 PrintStats:
-	mov edx, OFFSET msg_Total				; Print out the Total
+	; Display the stats
+
+	call Crlf
+
+	mov edx, OFFSET msg_Total					; Print out the Total
 	call WriteString
 	mov eax, total
 	call WriteInt
+	call Crlf
 
-	mov edx, OFFSET msg_Count				; Print out the Count
+	mov edx, OFFSET msg_Count					; Print out the Count
 	call WriteString
 	mov eax, count
 	call WriteInt
+	call Crlf
 
-	mov edx, OFFSET msg_Average				; Print out the Average
+	mov edx, OFFSET msg_Average					; Print out the Average
 	call WriteString
 	mov eax, average
 	call WriteInt
+	call Crlf
 	
-	mov edx, OFFSET msg_Remainder				;prints out the remainder
+	mov edx, OFFSET msg_Remainder				; Prints out the remainder
 	call Writestring
 	mov eax, remainder
 	call WriteInt
+	call Crlf
+
+	call Crlf
+	call Crlf
 	
+	mov flag_FirstInput, 1						; New round of inputs
 	call GetInput 
 
-PrintErrorNotInRange:
-	mov edx, OFFSET msg_err_NumRange
+
+Quit:
+	call Crlf
+
+	mov edx, OFFSET msg_Quit					; Print the quit message
 	call WriteString
-	call PrintStats					;Prints error message for input not in range 
 
-
+	call Crlf
 
 	exit
+
 main ENDP
 END main
